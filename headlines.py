63271541024970f32c198c07bdc73214ea5e2c14
Samuel Olaniyan <olaniyansamuel2015@gmail.com>
2022-02-24 14:55:23 +0000

    weather added

--- headlines.py
+++ headlines.py
@@ -1,4 +1,5 @@
-import feedparser
+import feedparser, json, urllib.parse, ssl
+import urllib.request as urllib2
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -8,18 +9,49 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              "fox": 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
+DEFAULTS = {'publication':'bbc',
+            'city': 'Aberdeen,UK'}
+
+WEATHER_URL= "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=b006a160163f0dedc0347b94a5998970"
+
 @app.route("/")
-def get_news():
-    query = request.args.get("publication")
+def home():
+    publication = request.args.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+    # get customized weather based on user input or default
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+    return render_template("home.html", articles=articles, weather=weather)
 
+def get_news(query):
     if not query or query not in RSS_FEEDS:
-        publication = "bbc"
-
+        publication = DEFAULTS["publication"]
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
+    return feed['entries']
 
-    return render_template("home.html", articles = feed['entries'])
+
+def get_weather(query):
+    query = urllib.parse.quote(query)
+    url = WEATHER_URL.format(query)
+    data = urllib2.urlopen(url).read()
+    parsed =json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description":parsed["weather"][0]["description"],
+                   "temperature": parsed["main"]["temp"],
+                   "city": parsed["name"],
+                   "country": parsed["sys"]["country"]
+                   }
+
+    return weather
+
+
 
 if __name__ == "__main__":
      app.run(port=5000, debug=True)
